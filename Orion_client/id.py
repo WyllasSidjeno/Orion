c0bd36d759e91a0f2d2a772d24e78e055bcdb3a8
Wyllas <perreaultchristopher1984@gmail.com>
2023-02-01 08:35:36 -0500

    Id : Documentation terminée

--- Orion_client/id.py
+++ Orion_client/id.py
@@ -1,13 +1,23 @@
-## -*- Encoding: UTF-8 -*-
+""" Module de géneration d'identifiant unique"""
+
+# -*- Encoding: UTF-8 -*-
 
 prochainid = 0
+""":type: int"""
 
 
 def get_prochain_id():
+    """Recupere le prochain id a utiliser.
+
+    :return: L'ID a utiliser.
+    :rtype: str
+    """
     global prochainid
     prochainid += 1
     return f'id_{prochainid}'
 
 
-if __name__ == '__main__':
-    test()
+#if __name__ == '__main__':
+#   test()
+# Removed main because it was not used and it was not working and
+# it was not needed for the game to work. Plus test() was not defined.
