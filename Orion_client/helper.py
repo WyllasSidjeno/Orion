4043d8481d477da9b0187aacda289b4d74ae37cb
Wyllas <perreaultchristopher1984@gmail.com>
2023-02-13 14:52:39 -0500

    AlwaysInt :: New updated version

--- Orion_client/helper.py
+++ Orion_client/helper.py
@@ -4,22 +4,8 @@ Ce module contient des methodes statiques pour calculer des points
 et des angles a partir de coordonnees cartesiennes.
 """
 import math
-
-
-class AlwaysInt(int):
-    """Classe qui permet de forcer un int a etre un int meme si on lui
-    donne un float.
-
-    Elle forme une boucle de surcharge sur l'ensemble des methodes de int
-    pour les appliquer sur l'objet lui-meme à l'exception des methodes
-    magiques (commencant par __) hormis les methodes magiques de division."""
-    for method in dir(int):
-        if not method.startswith('__'):
-            exec(f'def {method}(self, *args, **kwargs): '
-                 f'return int(self).__{method}__(*args, **kwargs)')
-        elif method.__contains__('div'):
-            exec(f'def {method}(self, *args, **kwargs): '
-                 f'return int(int(self).{method}(*args, **kwargs))')
+from typing import Any
+import functools
 
 class Helper(object):
     """Classe de geometrie 2D
@@ -78,3 +64,56 @@ class Helper(object):
         return distance
 
     calcDistance = staticmethod(calcDistance)
+
+
+class Inherited(type):
+    """Permet de reféfinir des méthodes héritées afin que le type de
+    retour soit celui de la sous-classe.
+    """
+    def __new__(
+            cls,
+            name: str,
+            bases: tuple[type, ...],
+            namespace: dict[str, Any]
+    ):
+
+        implemented: dict[type, list[str]] = namespace['_implements']
+
+        for base, methods in implemented.items():
+            for method_name in methods:
+                # Force early binding
+                def outer(method_name=method_name):
+                    method = getattr(base, method_name)
+                    @functools.wraps(method)
+                    def inner(self, *args, **kwargs):
+                        res = method.__call__(self, *args, **kwargs)
+                        reflected = f"__r{method_name[2:-2]}__"
+
+                        # Implement reflected methods
+                        if (
+                                res is NotImplemented
+                                and len(args) == 1 # Only binary ops
+                                and reflected in dir(args[0])
+                        ):
+                            res = getattr(args[0], reflected).__call__(self)
+
+                        return self.__class__(res)
+
+                    return inner
+                namespace[method_name] = outer(method_name)
+
+        return super().__new__(cls, name, bases, namespace)
+
+
+class AlwaysInt(int, metaclass=Inherited):
+    _implements = {
+        int: [
+                '__abs__', '__invert__', '__neg__', '__pos__',
+                '__ceil__', '__floor__', '__trunc__',
+        ]
+    }
+
+    for method in dir(int):
+        if method.startswith('__'):
+            if f"__r{method[2:-2]}__" in dir(int):
+                _implements[int].append(method)
