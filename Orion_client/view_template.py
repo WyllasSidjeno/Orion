95dc5cc86eef742962ada7d98f386706613de44c
Wyllas <perreaultchristopher1984@gmail.com>
2023-02-14 13:42:35 -0500

    Updated Docstrings

--- Orion_client/view_template.py
+++ Orion_client/view_template.py
@@ -39,7 +39,8 @@ class PlanetWindow(Frame):
         self.create_layout(self.planetInfo)
 
     def create_layout(self, planetInfo) -> None:
-        """Crée le layout de la fenetre"""
+        """Crée le layout de la fenetre, c'est à dire les frames et les
+        widgets"""
         self.configure(bg="dark grey", width=400, height=400)
 
         self.create_header(planetInfo["header"])
@@ -47,7 +48,12 @@ class PlanetWindow(Frame):
         self.create_side(planetInfo["output"])
 
     def create_header(self, planet_info) -> None:
-        """Crée le header de la fenetre"""
+        """Crée le header de la fenetre, la ou les informations identifiante
+        de la planete sont affichées
+
+        :param planet_info: Dictionnaire contenant
+        les informations de la planete
+        """
         self.headerFrame.place(relx=0, rely=0, relwidth=1, relheight=0.2)
 
         name_label = Label(self.headerFrame, text=planet_info["name"],
@@ -65,7 +71,9 @@ class PlanetWindow(Frame):
         owner_label.place(anchor="center", relx=0.5, rely=0.7)
 
     def create_main(self, planet_info) -> None:
-        """Crée le main de la fenetre"""
+        """Crée le main de la fenetre, là ou les bâtiments sont affichés
+
+        :param planet_info: Dictionnaire contenant"""
         self.mainFrame.place(relx=0, rely=0.2, relwidth=0.60, relheight=0.8)
 
         building_label = Label(self.mainFrame, text="Batiments",
@@ -85,7 +93,8 @@ class PlanetWindow(Frame):
                 building_frame.grid(row=i // 2, column=1, padx=10, pady=10)
 
     def create_side(self, planet_info) -> None:
-        """Crée le side de la fenetre"""
+        """Crée le side de la fenetre, là oú le rendement de la planete est
+        affiché"""
         self.sideFrame.place(relx=0.60, rely=0.2, relwidth=0.40, relheight=0.8)
 
         ressource_label = Label(self.sideFrame, text="Ressources",
