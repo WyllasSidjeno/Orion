466c2ab1ea793a27818542cda27c62e1b84c77dc
Wyllas <perreaultchristopher1984@gmail.com>
2023-02-16 17:49:16 -0500

    Refactorised controller
    Server, Lobby and Game controller mixed with a mother

--- Orion_client/controller.py
+++ Orion_client/controller.py
@@ -9,42 +9,174 @@ from Orion_client.view import App, GameView
 from Orion_client.orion_modele import Modele as Model
 
 
-class Controller():
+class Controller:
+    """Controller de l'application, incluant la connection au serveur"""
     model: Model
     def __init__(self):
+        """Initialisation du controller"""
+        self.frame = 1
         self.username: str = "p1"
-        self.actionsrecues: list[str] = []
+        self.server_actions: list[str] = []
         """Liste des actions reçues du serveur"""
-        self.actionsenvoyees: list[str] = []
+        self.player_actions: list[str] = []
         """Liste des actions à envoyées au serveur"""
         self.urlserveur: str = "http://127.0.0.1:8000"
 
         self.app = App(self.urlserveur, self.username)
 
-        self.bind_server_buttons()
+        self.user_controller = LobbyController(
+            self.username,
+            self.urlserveur,
+            self.app,
+            self.start_game)
 
         self.after_id = None
 
+    def start_game(self, joueurs):
+        """Start the game with the list of players"""
+        seed(12471)
+        ## Stop the update_lobby loop
+
+        listejoueurs = []
+        for i in joueurs:
+            listejoueurs.append(i[0])
+        self.model = Model(self, listejoueurs)
+        self.app.change_view(self.model)
+
+        self.user_controller = GameController(self.model, self.app)
+        self.server_controller = ServerController(
+            self.username,
+            self.urlserveur,
+            self.model,
+            self.pause_game,
+            self.unpause_game
+        )
+
+        self.loop()
+
+    def pause_game(self):
+        """Pause the game"""
+        self.user_controller.pause = True
+    def unpause_game(self):
+        """Unpause the game"""
+        self.user_controller.pause = False
+
+    def loop(self):
+        """Loop de l'application"""
+        temp = self.server_controller.update_actions(self.frame,
+                                                     self.player_actions)
+
+        if temp is not None:
+            self.update_model_actions(temp)
+        self.app.after(1000 // 60, self.loop)
+
+        self.user_controller.tick(self.frame)
+        self.frame += 1
+
+    def update_model_actions(self, actions):
+        """Update the model actions"""
+        self.model.ajouter_actions_a_faire(actions)
 
 
-    def call_server(self, url, params):
-        if params:
-            query_string = urllib.parse.urlencode(params)
-            data = query_string.encode("ascii")
-        else:
-            data = None
-        rep = urllib.request.urlopen(url, data, timeout=None)
-        reptext = rep.read()
-        rep = reptext.decode('utf-8')
-        rep = json.loads(rep)
-        return rep  # This is a dictionnary because of the json.loads
+
+class GameController:
+    """Controller de la partie"""
+    pause: bool
+    model: Model
+    def __init__(self, model: Model, app: App):
+        """Initialisation du controller"""
+        self.model = model
+        self.app = app
+        self.after_id = None
+        self.bind_game_buttons()
+        self.player_actions = []
+        self.server_actions = []
+        self.frame = 1
+        self.pause = False
+    def bind_game_buttons(self):
+        """Bind les boutons de la partie"""
+        self.app.view.canvas.bind("<MouseWheel>",
+                                  self.app.view.canvas.vertical_scroll)
+        self.app.view.canvas.bind("<Control-MouseWheel>", self.app.
+                                  view.canvas.horizontal_scroll)
+
+        self.app.view.canvas.bind("<Button-1>", self.print_tags)  # DEBUG
+
+    def print_tags(self, event):
+        """Print the tags of the current object"""
+        print(self.app.view.canvas.gettags("current"))
+        # xy
+        print(self.app.view.canvas.coords("current"))
+
+    def update_model_actions(self, actions: list[str]):
+        """Update the actions of the model"""
+        self.model.ajouter_actions_a_faire(actions)
+
+    def tick(self, frame):
+        """Update the game"""
+        if not self.pause:
+            self.model.jouer_prochain_coup(frame)
+            self.app.view.refresh(self.model)
+
+
+
+class ServerController:
+    def __init__(self, username, urlserveur,
+                 model, pause_game, unpause_game):
+        self.username: str = username
+        self.urlserveur: str = urlserveur
+        self.after_id = None
+
+        self.gameFrame = 0
+        self.gameFrameModulo = 2
+
+        self.model = model
+
+        self.pause_game = pause_game
+        self.unpause_game = unpause_game
+
+    def update_actions(self, cadre: int, actions : list[str]):
+        if cadre % self.gameFrameModulo == 0:
+            if actions:
+                actionsT = actions
+            else:
+                actionsT = None
+            actions = []
+            url = self.urlserveur + "/boucler_sur_jeu"
+            params = {"nom": self.username,
+                      "cadrejeu": cadre,
+                      "actionsrequises": actionsT}
+            try:
+                temp = call_server(url, params)
+                if "ATTENTION" in temp:
+                    print("ATTEND QUELQU'UN")
+                    self.pause_game()
+                else:
+                    self.model.ajouter_actions_a_faire(temp)
+            except urllib.error.URLError as e:
+                print("ERREUR ", cadre, e)
+                self.pause_game()
+        return actions
+
+
+class LobbyController:
+    model: Model
+    joueurs: list[list[str]]
+    def __init__(self, username, urlserveur, app, start_game):
+        self.username: str = username
+        self.urlserveur: str = urlserveur
+        self.app = app
+        self.after_id = None
+
+        self.start_game = start_game
+
+        self.bind_server_buttons()
 
     def connect_to_server(self, _):
         print("Connecting to server...")
-        url = self.urlserveur+"/tester_jeu"
+        url = self.urlserveur + "/tester_jeu"
         params = {"username": self.username}
-        temp = self.call_server(url, params)
-        print(temp)
+        temp = call_server(url, params)
         if temp[0][0]:
             str = temp[0][0]
             self.app.view.change_game_state(str)
@@ -55,9 +187,8 @@ class Controller():
 
     def restart_server(self, _):
         print("Restarting server...")
-        url = self.urlserveur+"/reset_jeu"
-        temp = self.call_server(url, None)
-        print(temp)
+        url = self.urlserveur + "/reset_jeu"
+        temp = call_server(url, None)
         if temp:
             self.app.view.change_game_state(temp[0][0])
 
@@ -65,7 +196,7 @@ class Controller():
         print("Starting game server...")
         url = self.urlserveur + "/creer_partie"
         params = {"nom": self.username}
-        temp = self.call_server(url, params)
+        temp = call_server(url, params)
         if temp:
             self.app.view.change_game_state(temp[0][0])
             self.app.after(1000, self.update_lobby)
@@ -75,39 +206,35 @@ class Controller():
         self.app.view.restart_button.bind("<Button-1>", self.restart_server)
 
     def update_lobby(self):
-        url = self.urlserveur+"/boucler_sur_lobby"
+        url = self.urlserveur + "/boucler_sur_lobby"
         params = {"nom": self.username}
-        temp = self.call_server(url, params)
-        print(temp)
+        temp = call_server(url, params)
         if 'courante' in temp:
-            self.start_game(temp)
+            self.start_game_signal(temp)
         else:
             self.joueurs = temp
             self.app.view.update_player_list(self.joueurs[0])
             self.after_id = self.app.after(1000, self.update_lobby)
             self.app.view.start_button.bind("<Button-1>",
-                                            self.start_game)
+                                            self.start_game_signal)
 
-    def start_game(self, _):
-        seed(12471)
-        ## Stop the update_lobby loop
+    def start_game_signal(self, _):
         self.app.after_cancel(self.after_id)
-        listejoueurs = []
-        for i in self.joueurs:
-            listejoueurs.append(i[0])
-        self.model = Model(self, listejoueurs)
-        self.app.change_view(self.model)
 
-        self.app.view.canvas.bind("<MouseWheel>",
-                                  self.app.view.canvas.vertical_scroll)
-        self.app.view.canvas.bind("<Control-MouseWheel>", self.app.
-                                  view.canvas.horizontal_scroll)
+        self.start_game(self.joueurs)
 
-        self.app.view.canvas.bind("<Button-1>", self.print_tags) # DEBUG
-    def print_tags(self, event):
-        print(self.app.view.canvas.gettags("current"))
-        # xy
-        print(self.app.view.canvas.coords("current"))
+
+def call_server(url, params):
+    if params:
+        query_string = urllib.parse.urlencode(params)
+        data = query_string.encode("ascii")
+    else:
+        data = None
+    rep = urllib.request.urlopen(url, data, timeout=None)
+    reptext = rep.read()
+    rep = reptext.decode('utf-8')
+    rep = json.loads(rep)
+    return rep  # This is a dictionnary because of the json.loads
 
 
 
