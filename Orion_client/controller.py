5017b526b7123a67ef71dc91237a49f81a014357
Wyllas <perreaultchristopher1984@gmail.com>
2023-02-15 20:39:29 -0500

    Added Model link and Start Game

--- Orion_client/controller.py
+++ Orion_client/controller.py
@@ -3,12 +3,14 @@ import urllib
 import urllib.error
 import urllib.parse
 import urllib.request
+from random import random, seed
 
 from Orion_client.view import App
 from Orion_client.orion_modele import Modele as Model
 
 
 class Controller():
+    model: Model
     def __init__(self):
         self.username: str = "p1"
         self.actionsrecues: list[str] = []
@@ -20,16 +22,8 @@ class Controller():
         self.app = App(self.urlserveur, self.username)
 
         self.bind_server_buttons()
-        self.update_lobby()
 
-    def connect_to_server(self, _):
-        print("Connecting to server...")
-        url = self.urlserveur+"/tester_jeu"
-        params = {"username": self.username}
-        temp = self.call_server(url, params)
-        print(temp)
-        if temp:
-            self.app.view.change_game_state(temp[0][0])
+
 
     def call_server(self, url, params):
         if params:
@@ -43,6 +37,20 @@ class Controller():
         rep = json.loads(rep)
         return rep  # This is a dictionnary because of the json.loads
 
+    def connect_to_server(self, _):
+        print("Connecting to server...")
+        url = self.urlserveur+"/tester_jeu"
+        params = {"username": self.username}
+        temp = self.call_server(url, params)
+        print(temp)
+        if temp[0][0]:
+            str = temp[0][0]
+            self.app.view.change_game_state(str)
+            if str == "dispo":
+                self.start_game_server()
+            elif str == "attente":
+                pass
+
     def restart_server(self, _):
         print("Restarting server...")
         url = self.urlserveur+"/reset_jeu"
@@ -51,9 +59,17 @@ class Controller():
         if temp:
             self.app.view.change_game_state(temp[0][0])
 
+    def start_game_server(self):
+        print("Starting game server...")
+        url = self.urlserveur + "/creer_partie"
+        params = {"nom": self.username}
+        temp = self.call_server(url, params)
+        if temp:
+            self.app.view.change_game_state(temp[0][0])
+            self.app.after(1000, self.update_lobby)
+
     def bind_server_buttons(self):
         self.app.view.connect_button.bind("<Button-1>", self.connect_to_server)
-
         self.app.view.restart_button.bind("<Button-1>", self.restart_server)
 
     def update_lobby(self):
@@ -64,17 +80,22 @@ class Controller():
             self.start_game(temp)
         else:
             self.joueurs = temp
-            self.app.view.update_player_list(self.joueurs)
+            self.app.view.update_player_list(self.joueurs[0])
             self.app.after(1000, self.update_lobby)
+            self.app.view.start_button.bind("<Button-1>",
+                                            self.start_game)
 
-    def start_game(self, temp):
-        pass
+    def start_game(self, _):
 
+        print("Starting game...")
+        seed(12471)
 
+        listejoueurs = []
 
+        for i in self.joueurs:
+            listejoueurs.append(i[0])
 
-
-
+        self.model = Model(self,listejoueurs)
 
 if __name__ == "__main__":
     controller = Controller()
