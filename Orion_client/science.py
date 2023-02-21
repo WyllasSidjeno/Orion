f212beb790703ea6cb1f6ee9a4af49b10830fbbd
bibz43 <68484992+Juliettevincent@users.noreply.github.com>
2023-02-21 08:12:25 -0500

    Update science.py

--- Orion_client/science.py
+++ Orion_client/science.py
@@ -3,23 +3,46 @@
 # from __future__ import annotations
 if __name__ == "__main__":
     from tkinter import Tk
-
-    root = Tk()
-    frame = CustomFrame(root)
-    frame.pack()
-    root.mainloop()
+    from Orion_client.helper import AlwaysInt
+    # root = Tk()
+    # frame = CustomFrame(root)
+    # frame.pack()
+    # root.mainloop()
 
     class ScienceTree:
-        # AlwaysInt unlocked_ship_tier, unlocked_building_tier, acquired_science <- global
+        """Classe qui contient l'arbre de science"""
+        niveau_vaisseau_dispo = AlwaysInt(0)  # niveau de vaisseau débloqué
+        niveau_batiment_dispo = AlwaysInt(0)  # niveau de bâtiment débloqué
+        science_acquise = AlwaysInt(0)
+        sciences: dict = {}
+
         def __init__(self):
             """Constructeur de la classe"""
             self.root = Tk()
-            self.sciences = ["", "", ""] # trouver des exemples de sciences
-            # toutes les sciences dispo
+            self.science_points = 0 # points de science
 
-       # def show_science:
+        def show_science(self):
+            """Affiche les sciences"""
+            self.science_tree.pack()
+            for science in self.science_tree:
+                self.science_bloquer()
             # fenetre science & self.science
 
-        # def on_click_select:
+        def science_bloquer(self):
+            """Si : Affiche la case d'une science bloquée
+            Sinon : Affiche la case d'une science débloquée"""
+            pass
 
-        # def is_unlockable:
+        def science_debloquer(self):
+            """Affiche la case d'une science débloquée"""
+            pass
+
+        def is_unlockable(self):
+            """Vérifie si la science est débloquable
+            si : science précédente débloquée et points de science suffisant"""
+            pass
+
+        def on_click_select(self):
+            """Affiche une fenetre pour acheter la science"""
+            pass
+
