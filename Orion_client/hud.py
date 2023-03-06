f96466ccbbf3ae08c5ffe3b7b376ec5285d10098
Sl1mmy <noebousquet2004@gmail.com>
2023-03-06 08:22:56 -0500

    removed hud.py

--- Orion_client/hud.py
+++
@@ -1,7 +0,0 @@
-"""Module qui contient la classe Hud"""
-import tkinter as tk
-from __future__ import annotations
-from typing import Any
-from helper import AlwaysInt
-from ressource import Ressource
-
