ad53b01b3ac3698c4cd118950b127d10176888d8
bibz43 <68484992+Juliettevincent@users.noreply.github.com>
2023-03-21 10:21:38 -0400

    is_clickable function

--- Orion_client/science.py
+++ Orion_client/science.py
@@ -10,9 +10,10 @@ class ArbreScience:
     niveau_vaisseau_dispo = AlwaysInt(0)  # niveau de vaisseau débloqué
     niveau_batiment_dispo = AlwaysInt(0)  # niveau de bâtiment débloqué
     science_acquise = AlwaysInt(0)
-    sciences: dict = {'science_0': [0, True, 4], 'science_1': [1, True, 5], 'science_2': [2, False, 8], 'science_3':
-        [3, False, 9], 'science_4': [4, False, 10], 'science_5': [5, False, 11], 'science_6': [6, False, 13],
-                      'science_7': [7, False, 15]}
+    sciences: dict = {'science_0': [0, True, 4, "available"], 'science_1': [1, True, 5, "available"], 'science_2':
+        [2, False, 8, "unlockable"], 'science_3': [3, False, 9, "unlockable"], 'science_4': [4, False, 10, "blocked"]
+        , 'science_5': [5, False, 11, "blocked"], 'science_6': [6, False, 13, "blocked"], 'science_7': [7, False, 15,
+                                                                                                        "blocked"]}
 
     def __init__(self):
         """Constructeur de la classe"""
@@ -51,16 +52,17 @@ class ArbreScience:
 
 class Controller:
     """Classe qui contient les méthodes de l'arbre de science"""
-    sciences: dict = {'science_0': [0, True, 4], 'science_1': [1, True, 5], 'science_2': [2, False, 8], 'science_3':
-        [3, False, 9], 'science_4': [4, False, 10], 'science_5': [5, False, 11], 'science_6': [6, False, 13],
-                      'science_7': [7, False, 15]}
+    sciences: dict = {'science_0': [0, True, 4, "available"], 'science_1': [1, True, 5, "available"], 'science_2':
+        [2, False, 8, "unlockable"], 'science_3': [3, False, 9, "unlockable"], 'science_4': [4, False, 10, "blocked"]
+        , 'science_5': [5, False, 11, "blocked"], 'science_6': [6, False, 13, "blocked"], 'science_7': [7, False, 15,
+                                                                                                        "blocked"]}
 
     def __init__(self):
         self.root = root
         self.science_points = AlwaysInt(6)  # points de science
         self.sortedDict = sorted(self.sciences.items())
-        self.maxColumn = AlwaysInt(0)
-        self.maxRow = AlwaysInt(4)
+        self.maxRow = AlwaysInt(0)
+        self.maxColumn = AlwaysInt(4)
         self.root.config(bg="#36393f")
 
     def show_science(self, data):
@@ -69,20 +71,20 @@ class Controller:
             prix = data.sciences.get(science)[2]
 
             if self.science_bloquer(f"{science}", data):
-                print(self.science_bloquer(f"{science}", data))
                 con_frame = self.science_debloquer()
                 lbl = tk.Label(con_frame, text=f"{science} prix : {prix}", height=5, width=15, bg="orange")
             else:
                 con_frame = tk.Frame(self.root, bg="#2f3136", highlightbackground="black", highlightthickness=3)
                 lbl = tk.Label(con_frame, text=f"{science} prix : {prix}", height=5, width=15, bg="#2f3136")
 
-            if i % self.maxRow == 0 and i != 0:
-                self.maxColumn += 1
+            if i % self.maxColumn == 0 and i != 0:
+                self.maxRow += 1
 
-            setattr(self, f"{science}_{i}", con_frame.grid(row=i % self.maxRow, column=self.maxColumn, padx=2, pady=2))
-            setattr(self, f"{science}_{i}", lbl.grid(row=i % self.maxRow, column=self.maxColumn, padx=2, pady=2))
+            setattr(self, f"{science}_{i}", con_frame.grid(row=self.maxRow, column=i % self.maxColumn, padx=2, pady=2))
+            setattr(self, f"{science}_{i}", lbl.grid(row=self.maxRow, column=i % self.maxColumn, padx=2, pady=2))
 
-            lbl.bind("<Button-1>", self.on_click_select)
+            if self.is_clickable(f"{science}", data):
+                lbl.bind("<Button-1>", self.on_click_select)
             lbl.grid(sticky='nsew')
 
     def science_bloquer(self, science, data):
@@ -99,17 +101,26 @@ class Controller:
         con_frame = tk.Frame(self.root, bg="orange", highlightbackground="#F7CE25", highlightthickness=3)
         return con_frame
 
+    def is_clickable(self, science, data):
+        """Verifie si la science peut etre cliquer"""
+        if data.sciences.get(science)[3] == "blocked":
+            return False
+        else:
+            return True
+
     def is_unlockable(self):
-        """Vérifie si la science est débloquable
-        si : science précédente débloquée et points de science suffisant"""
+        """Verifie chaque row si les sciences sont available"""
         pass
+
+    def buy_science(self, event, science, data):
+        """Achete la science"""
+        data.sciences.get(science)[3] = "available"
 
     def on_click_select(self, event):
         """Affiche une fenetre pour acheter la science
         button : acheter
         button : annuler"""
         # appeler la fonction is_unlockable et science_bloquer
-
         buyScience = tk.Frame(self.root, bg="#2f3136", highlightbackground="#F7CE25", highlightthickness=2)
         buyLabel = tk.Label(buyScience, text="Buy science?", fg="#F7CE25", bg="#2f3136", font=("Arial", 10))
 
@@ -119,7 +130,8 @@ class Controller:
         OuiBouton = tk.Button(buyScience, text="Oui", width=10, height=1, bg="#cbff00")
         annulerBouton = tk.Button(buyScience, text="Annuler", width=10, height=1, bg="#ff5200",
                                   command=buyScience.destroy)
-
+        OuiBouton.bind("<Button-1>", self.buy_science)
+        # todo: ajouter les parametres
         OuiBouton.grid(row=1, column=0, sticky="nsew")
         annulerBouton.grid(row=1, column=1, sticky="nsew")
         buyLabel.grid(row=0, column=0, sticky="nsew")
