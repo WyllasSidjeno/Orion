244258c8a3c7a1013844acdc0051f603bb6111eb
Wyllas <perreaultchristopher1984@gmail.com>
2023-04-17 19:35:53 -0400

    Removed Tk calls for better imports reading

--- Orion_client/science.py
+++ Orion_client/science.py
@@ -1,10 +1,10 @@
 """Module qui contient la classe Science et Science Tree"""
-import tkinter as tk
+from tkinter import Frame, Label, Button, Tk
 from Orion_client.helpers.helper import AlwaysInt
 from Orion_client.view.view_common_ressources import *
 
 
-class ArbreScience(tk.Frame):
+class ArbreScience(Frame):
     """Classe qui contient l'arbre de science"""
     niveau_vaisseau_dispo = AlwaysInt(0)  # niveau de vaisseau débloqué
     niveau_batiment_dispo = AlwaysInt(0)  # niveau de bâtiment débloqué
@@ -21,7 +21,7 @@ class ArbreScience(tk.Frame):
     }
 
     choix_temporaire: str
-    buyscience: tk.Frame
+    buyscience: Frame
 
     def __init__(self, master, width: int = 500, height: int = 250, **kwargs):
         """Constructeur de la classe"""
@@ -30,12 +30,12 @@ class ArbreScience(tk.Frame):
         self.grid_propagate(False)
         self.science_points = 10
 
-        self.title = tk.Label(self, text="Arbre de science", bg=hexGrey,
-                              fg="white", font=("Arial", 20))
+        self.title = Label(self, text="Arbre de science", bg=hexGrey,
+                           fg="white", font=("Arial", 20))
 
         self.title.grid(row=0, column=0, sticky="nsew")
 
-        self.science_frame = tk.Frame(self, bg=hexGrey)
+        self.science_frame = Frame(self, bg=hexGrey)
         self.science_frame.grid(row=1, column=0, sticky="nsew")
 
         self.show_science(ArbreScience.sciences)
@@ -46,22 +46,22 @@ class ArbreScience(tk.Frame):
         max_column = 4
         for i, science in enumerate(sciences):
             prix = sciences.get(science)[0]
-            con_frame = tk.Frame(
+            con_frame = Frame(
                 self.science_frame, bg=hexGrey,
                 highlightbackground="black",
                 highlightthickness=3
             )
 
             if self.science_acquises(science, sciences):
-                lbl = tk.Label(con_frame, text=f"{science} prix : {prix}",
-                               height=5, width=15, bg=hexDarkGreen)
+                lbl = Label(con_frame, text=f"{science} prix : {prix}",
+                            height=5, width=15, bg=hexDarkGreen)
             else:
-                lbl = tk.Label(con_frame, text=f"{science} prix : {prix}",
-                               height=5, width=15, bg=hexGreyGreen)
+                lbl = Label(con_frame, text=f"{science} prix : {prix}",
+                            height=5, width=15, bg=hexGreyGreen)
 
             if self.science_achetable(science, sciences):
-                lbl = tk.Label(con_frame, text=f"{science} prix : {prix}",
-                               height=5, width=15, bg=hexYellowGreen)
+                lbl = Label(con_frame, text=f"{science} prix : {prix}",
+                            height=5, width=15, bg=hexYellowGreen)
 
             if i % max_column == 0 and i != 0:
                 max_row += 1
@@ -81,7 +81,7 @@ class ArbreScience(tk.Frame):
     @staticmethod
     def science_acquises(science: str, sciences: dict) -> bool:
         """Verifie si la science est acquise"""
-        #Todo : Modele
+        # Todo : Modele
         if sciences[science][1] == "acquise":
             return True
         return False
@@ -90,7 +90,7 @@ class ArbreScience(tk.Frame):
         """Si points suffisant, niveau suffisant et is_unlockable
          : Affiche la case d'une science bloquée
          Sinon : Affiche la case d'une science débloquée"""
-        #Todo : Modele
+        # Todo : Modele
         if self.science_points >= sciences[science][0]:
             return True
         return False
@@ -110,23 +110,23 @@ class ArbreScience(tk.Frame):
 
         self.choix_temporaire = event.widget.cget("text")
 
-        self.buyscience = tk.Frame(self, bg=hexDarkGrey,
-                                   highlightbackground=hexBrightYellow,
-                                   highlightthickness=2)
-        buy_label = tk.Label(self.buyscience, text="Buy science?",
-                             fg=hexBrightYellow, bg=hexDarkGrey,
-                             font=("Arial", 10))
+        self.buyscience = Frame(self, bg=hexDarkGrey,
+                                highlightbackground=hexBrightYellow,
+                                highlightthickness=2)
+        buy_label = Label(self.buyscience, text="Buy science?",
+                          fg=hexBrightYellow, bg=hexDarkGrey,
+                          font=("Arial", 10))
 
         self.buyscience.place(relx=0.5, rely=0.8, anchor="center")
         buy_label.place(relx=0.06, rely=0.65)
 
-        oui_bouton = tk.Button(self.buyscience, text="Oui", width=10,
-                               height=1, bg=hexBrightGreen
-                               )
-        annuler_bouton = tk.Button(self.buyscience, text="Annuler", width=10,
-                                   height=1, bg=hexRed,
-                                   command=self.buyscience.destroy
-                                   )
+        oui_bouton = Button(self.buyscience, text="Oui", width=10,
+                            height=1, bg=hexBrightGreen
+                            )
+        annuler_bouton = Button(self.buyscience, text="Annuler", width=10,
+                                height=1, bg=hexRed,
+                                command=self.buyscience.destroy
+                                )
         oui_bouton.bind("<Button-1>", self.buy_science)
 
         oui_bouton.grid(row=1, column=0, sticky="nsew")
@@ -135,7 +135,7 @@ class ArbreScience(tk.Frame):
 
 
 if __name__ == "__main__":
-    root = tk.Tk()
+    root = Tk()
     root.title("Science tree")
     root.geometry("800x600")
     arbre = ArbreScience(root)
