d7b5e0c0e1c15215aa66f01d27a38405fcbbc472
Wyllas <perreaultchristopher1984@gmail.com>
2023-05-22 15:10:43 -0400

    Finished Ship Desc

--- Documentation/js/main.js
+++ Documentation/js/main.js
@@ -18,27 +18,21 @@ window.addEventListener('load', function() {
     content = document.getElementById('MainBar');
     tempList = content.querySelectorAll('button');
     tempList[0].addEventListener('click', function() {
-        // go to index.html
         window.location.href = "index.html";
     });
     tempList[1].addEventListener('click', function() {
-        // go to index.html
         window.location.href = "building.html";
     });
     tempList[2].addEventListener('click', function() {
-        // go to index.html
         window.location.href = "ship.html";
     });
     tempList[3].addEventListener('click', function() {
-        // go to index.html
         window.location.href = "ressource.html";
     });
     tempList[4].addEventListener('click', function() {
-        // go to index.html
         window.location.href = "research.html";
     });
     tempList[5].addEventListener('click', function() {
-        // go to index.html
         window.location.href = "commands.html";
     });
 });
